6a67fcbf26304516368ab836999bd998d8f699cd
Eniac <Eniac@Eniac>
2019-08-19 00:20:44 -0500

    first commit.

---
+++ src/main/java/com/chipilinsoft/EFactureApplication.java
@@ -0,0 +1,13 @@
+package com.chipilinsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFactureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFactureApplication.class, args);
+	}
+
+}
